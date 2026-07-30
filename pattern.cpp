a9a43ea5623603e6674a946de2ac5e8aa0267b27
Roshan Yadav <iroshan3008@gmail.com>
2026-07-30 23:49:53 +0530

    Solved pattern problems locally

---
+++ pattern.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+    int x;
+    cout << "Enter the number :";
+    cin >> x;
+    for(int i=0 ; i<x;i++){
+        for(int j =0 ; j<x;j++){
+            cout<< "*";
+        }
+    cout<< endl;
+    }
+
+    return 0;
+}
---
+++ tuf_pattern7.cpp
@@ -0,0 +1,32 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+    int n;
+    cin>>n;
+
+    for(int i=0; i<n;i++){
+
+        // print space
+        for(int j= 0 ; j<n-i-1;j++){
+            cout<< " ";
+        }
+
+        // print stars 
+        for(int j=0 ; j<2*i+1; j++){
+            cout<< "*";
+        }
+
+
+        // print space
+        for(int j= 0 ; j<n-i-1;j++){
+            cout<< " ";
+        }
+ 
+
+        
+    cout<< endl;
+    }
+
+    return 0;
+}
