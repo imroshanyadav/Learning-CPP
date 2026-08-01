f02bd6281eb5fc63db2642fc17f770ead1580af3
Roshan Yadav <iroshan3008@gmail.com>
2026-08-01 21:34:57 +0530

    new remaining problems

---
+++ tuf_pattern4.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+
+    int n ;
+    cin>>n;
+    int c =-1;
+
+    for (int i =0 ; i<n;i++){
+        c+=1;
+        
+        for (int j =0 ;j<=i;j++){
+            cout<< c;
+        }cout<< endl;
+    }return 0;
+}
