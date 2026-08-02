30e1cb69000c3534f933a2c40354cdd12a18293d
Roshan Yadav <iroshan3008@gmail.com>
2026-08-02 22:42:20 +0530

    tuf_pattern_19

---
+++ tuf_pattern19.cpp
@@ -0,0 +1,42 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+    int n ;
+    cin>>n;
+    int space = 0;
+     for(int i = n ; i>=1 ; i--){
+        for (int j =1 ; j<=i; j++){
+            cout << "*";
+
+        }for (int j =1; j<=space;j++){
+            cout << " ";
+        }for (int j =i ; j>=1; j--){
+            cout << "*";
+
+        }
+        cout<<endl;
+        space +=2;
+    }
+    int spaces = 2*(n-1);
+    for(int i = 1 ; i<= n ; i++){
+        for (int j =1 ; j<=i; j++){
+            cout << "*";
+
+        }for (int j =1; j<=spaces;j++){
+            cout << " ";
+        }for (int j =i ; j>=1; j--){
+            cout << "*";
+
+        }
+        cout<<endl;
+        spaces -=2;
+    }
+
+
+
+
+    return 0;
+}
+
+
