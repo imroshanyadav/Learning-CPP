0735c29e61bb7946443a41d4979874b1e3e63267
Roshan Yadav <iroshan3008@gmail.com>
2026-08-02 22:21:25 +0530

    tuf_pattern18_solve

---
+++ tuf_pattern18.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+    int n;
+    cin>>n;
+    char x = 'E';
+
+    for(int i =0 ; i<n ; i++){
+        char x = 'E'-i;
+        for (int j =0; j<=i ; j++){
+            cout << x;
+            x++;
+            
+        }cout << endl;
+
+    }return 0;
+}
